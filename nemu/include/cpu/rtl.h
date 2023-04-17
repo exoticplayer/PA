fa51bb6663695a7d3028b514f8597ccfabe8593a
tracer-ics2017 <tracer@njuics.org>
2023-04-17 08:04:05 -0700

    >  compile 2011743 gyk Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  08:04:05 up  7:45,  1 user,  load average: 0.26, 0.23, 0.24 d3994ebeafed0407a84f9696552ac2cc5cfb6aba

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -113,10 +113,12 @@ static inline void rtl_sr(int r, int width, const rtlreg_t* src1) {
 
 #define make_rtl_setget_eflags(f) \
   static inline void concat(rtl_set_, f) (const rtlreg_t* src) { \
-    TODO(); \
+    /*TODO();*/ \
+    cpu.eflags.f=*src;\
   } \
   static inline void concat(rtl_get_, f) (rtlreg_t* dest) { \
-    TODO(); \
+   /* TODO(); */\
+    *dest=cpu.eflags.f;\
   }
 
 make_rtl_setget_eflags(CF)
@@ -142,13 +144,17 @@ static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
 static inline void rtl_push(const rtlreg_t* src1) {
   // esp <- esp - 4
   // M[esp] <- src1
-  TODO();
+  // TODO();
+  rtl_subi(&cpu.esp, &cpu.esp, 4);
+  rtl_sm(&cpu.esp, 4, src1);
 }
 
 static inline void rtl_pop(rtlreg_t* dest) {
   // dest <- M[esp]
   // esp <- esp + 4
-  TODO();
+  //TODO();
+  rtl_lm(dest, &cpu.esp, 4);
+  rtl_addi(&cpu.esp, &cpu.esp, 4);
 }
 
 static inline void rtl_eq0(rtlreg_t* dest, const rtlreg_t* src1) {
@@ -168,19 +174,26 @@ static inline void rtl_neq0(rtlreg_t* dest, const rtlreg_t* src1) {
 
 static inline void rtl_msb(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- src1[width * 8 - 1]
-  TODO();
+  //TODO();
+  rtl_shri(dest, src1, width*8-1);
+  rtl_andi(dest, dest, 0x1);
 }
 
 static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
   // eflags.ZF <- is_zero(result[width * 8 - 1 .. 0])
-  TODO();
+  //TODO();
+  rtl_andi(&t0, result, (0xffffffffu >> (4-width)*8));
+  rtl_eq0(&t0, &t0);
+  rtl_set_ZF(&t0);
 }
 
 static inline void rtl_update_SF(const rtlreg_t* result, int width) {
   // eflags.SF <- is_sign(result[width * 8 - 1 .. 0])
-  TODO();
+  // TODO();
+  assert(result != &t0);
+  rtl_msb(&t0, result, width);
+  rtl_set_SF(&t0);
 }
-
 static inline void rtl_update_ZFSF(const rtlreg_t* result, int width) {
   rtl_update_ZF(result, width);
   rtl_update_SF(result, width);
--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -6,3 +6,25 @@ make_EHelper(operand_size);
 
 make_EHelper(inv);
 make_EHelper(nemu_trap);
+
+
+make_EHelper(call);
+make_EHelper(push);
+make_EHelper(ret);
+make_EHelper(sub);
+make_EHelper(xor);
+make_EHelper(pop);
+
+make_EHelper(and);
+make_EHelper(sbb);
+make_EHelper(adc);
+make_EHelper(or);
+make_EHelper(add);
+make_EHelper(cmp);
+make_EHelper(rol);
+make_EHelper(shl);
+make_EHelper(shr);
+make_EHelper(sar);
+make_EHelper(nop);
+make_EHelper(jmp);
+
